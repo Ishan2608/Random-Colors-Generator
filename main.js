55e3e61fbf3023e5a05c416a3194746d0fb496ff
IshanRastogi2608 <ishanrastogi26@gamil.com>
2022-12-30 12:47:52 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,83 @@
+window.addEventListener("DOMContentLoaded", start_script)
+
+let codes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+let color_string = "#";
+
+
+const generate_btn = document.querySelector('.left .btn');
+const right_panel = document.querySelector('.right');
+const color_qty = document.querySelector('#colors-qty');
+const codes_div = document.querySelector('.codes.divisions');
+const hex_code = document.querySelector('.codes .hex-code');
+const rgb_code = document.querySelector('.codes .rgb-code');
+const status_text = document.querySelector('.status');
+let color_boxes = null;
+
+hex_code.addEventListener('click', copy_to_clipboard);
+rgb_code.addEventListener('click', copy_to_clipboard);
+
+function start_script(){
+    generate_btn.addEventListener('click', ()=>{
+        destroy(right_panel)
+        codes_div.classList.add('hide');
+        let qty = color_qty.value;
+        for (let quantity = 0; quantity < qty; quantity++) {
+            let color_box = document.createElement('div');
+            color_box.classList.add('color-box');
+            color_box.style.backgroundColor = generate_colour();
+            right_panel.appendChild(color_box);
+            color_string = "#";
+        }
+
+        color_boxes = document.querySelectorAll('.color-box');
+        // console.log(color_boxes);
+        color_boxes.forEach(box =>{
+            box.addEventListener("click", show_color_codes);
+        })
+    })
+}
+
+
+function generate_colour(){
+    for(i=0; i<6; i++){
+        let index = Math.floor(Math.random() * 16);
+        color_string += `${codes[index]}`;
+    }
+    return color_string;
+}
+
+
+function destroy(elem){
+    while(elem.firstChild){
+        elem.removeChild(elem.lastChild);
+    }
+}
+
+function show_color_codes(event){
+    let color_code_string = event.target.style.backgroundColor;
+    let split_codes = color_code_string.slice(4, color_code_string.length-1).split(', ');
+    let r = split_codes[0];
+    let g = split_codes[1];
+    let b = split_codes[2];
+    hex_code.textContent = "";
+    hex_code.textContent = rgbToHex(r, g, b);
+    rgb_code.textContent = event.target.style.backgroundColor;
+    codes_div.classList.remove('hide');
+    
+}
+
+
+// Code Credit to StackOverFlow for the rgbToHex() function
+function rgbToHex(r, g, b) {
+    return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+}
+
+function copy_to_clipboard(event){
+    navigator.clipboard.writeText(event.target.textContent);
+    status_text.textContent = "Copied to Clipboard";
+    status_text.classList.remove('hide');
+    setTimeout(()=>{
+        status_text.classList.add('hide');
+    }, 2000)
+}
